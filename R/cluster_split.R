671bc0496b7c2859c98c74bacae25026d85cfc88
ShixiangWang <wangshx@shanghaitech.edu.cn>
2020-07-19 17:44:25 +0800

    Add split fun

---
+++ R/cluster_split.R
@@ -0,0 +1,25 @@
+#' Split Cluster Sequence into List
+#'
+#' @param x a named integer vector from `hclust` etc.
+#' @param s default is `NULL`, when the `x` is block cluster sequence, set this
+#' to a sequence vector.
+#' @param block_size block size used to split, only used when `s` is not `NULL`.
+#'
+#' @return a `list`.
+#' @export
+sh_cluster_split <- function(x, s = NULL, block_size = 10) {
+  if (startsWith(x[1], "block")) {
+    cluster_blocks <- x
+    dt <- dplyr::tibble(
+      block = names(cluster_blocks),
+      cluster = as.character(cluster_blocks),
+      seq = sigminer:::chunk2(s, ceiling(length(s) / block_size))
+    ) %>%
+      tidyr::unnest("seq") %>%
+      data.table::as.data.table()
+
+    split(dt$seq, dt$cluster)
+  } else {
+    split(names(x), as.integer(x))
+  }
+}
